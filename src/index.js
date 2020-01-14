c823c59c7ed064817bb7e6cac978c58d0a9a980f
github username <github email address>
2020-01-13 21:12:03 -0500

    Added movie constructor for attribiutes will need to add image to model

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 fetch('http://localhost:3000/movies')
 .then(resp => resp.json())
+.then(movies => {
+  movies.forEach
+  }
+})
--- src/movie.js
+++ src/movie.js
@@ -1,3 +1,10 @@
 class Movie {
-  
+  constructor(id, title, description, genre, image, stars){
+  this.id = id
+  this.title = title
+  this.description = description
+  this.genre = genre
+  this.image = image
+  this.stars = stars
+  }
 }
