e9c0b76cf3e728900a33d19f01919f6e8c01aaba
github username <github email address>
2020-01-14 10:51:12 -0500

    Created delete method click event and event listener

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 fetch('http://localhost:3000/movies')
 .then(resp => resp.json())
 .then(movies => {
-  movies.forEach
-  }
+  movies.forEach ( movie => {
+    const {id, title, description, genre, image, stars} = movie
+    new Movie(id, title, description, genre, image, stars)
+    })
 })
--- src/movie.js
+++ src/movie.js
@@ -6,5 +6,38 @@ class Movie {
   this.genre = genre
   this.image = image
   this.stars = stars
+  this.render()
+  }
+
+  delete() {
+    const id = e.target.dataset.id
+    fetch(`http://localhost:3000/movies/${id}`,{
+      method: "DELETE",
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then (()=>{
+      e.target.parentElement.remove()
+    })
+  }
+
+
+  render() {
+    const movieContainer = document.getElementById('movie-container')
+    const movieCard = document.createElement('div')
+    movieCard.classList.add('movie-card')
+    movieCard.id = this.id
+    movieCard.innerHTML = `
+        <h2>${this.title}</h2>
+        <img src="${this.image}" />
+        <p>Description: ${this.description}</p>
+        <p>Genre: ${this.genre}</p>
+        <p>Stars:</p>
+        <button data-id="${this.id}" class="delete">Delete Movie?</button
+      `
+    movieContainer.appendChild(movieCard)
+    movieCard.addEventListener('click', e => {
+      if (e.target.className === 'delete') this.delete(e)
+    })
   }
 }
