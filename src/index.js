903952140e494957c083d0f2242883559141bea6
github username <github email address>
2020-01-15 10:25:29 -0500

    Added new function and fetch call to be able to create object/movie

--- src/index.js
+++ src/index.js
@@ -6,3 +6,23 @@ fetch('http://localhost:3000/movies')
     new Movie(id, title, description, genre, image, stars)
   })
 })
+
+document.querySelector('form').addEventListener('submit', addMovie)
+
+function addMovie(e) {
+  e.preventDefault()
+  const data = {
+    'title': e.target.title.value
+    'description': e.target.description.value
+    'genre': e.target.genre.value
+    'stars': e.target.stars.value
+    'Image': e.target.img.value
+  }
+  fetch('http://localhost:3000/movies', {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ data })
+  })
+}
