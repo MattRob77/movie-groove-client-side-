29ae260c3ce42837f6ccba83b12acf3945cb8aae
github username <github email address>
2020-01-16 09:41:23 -0500

    Updated function for adding movie

--- src/index.js
+++ src/index.js
@@ -18,29 +18,6 @@ function addMovie(e) {
     'stars': e.target.stars.value,
     'Image': e.target.img.value
   }
-  fetch('http://localhost:3000/movies', {
-    method: "POST",
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  }).then((movie)=> {
-    const {id, title, description, genre, image, stars} = movie
-    new Movie(id, title, description, genre, image, stars)
-  })
-}
-
-document.querySelector('form').addEventListener('submit', addMovie)
-
-function addMovie(e) {
-  e.preventDefault()
-  const data = {
-    'title': e.target.title.value,
-    'description': e.target.description.value,
-    'genre': e.target.genre.value,
-    'stars': e.target.stars.value,
-    'Image': e.target.img.value
-  }
   fetch('http://localhost:3000/movies/', {
     method: "POST",
     headers: {
