414dc3f4bfe7b40857b5eac55be29ba2a9839184
github username <github email address>
2020-01-16 09:39:13 -0500

    Created function for creating a new movie and a fetch call from local host movies

--- src/index.js
+++ src/index.js
@@ -29,3 +29,27 @@ function addMovie(e) {
     new Movie(id, title, description, genre, image, stars)
   })
 }
+
+document.querySelector('form').addEventListener('submit', addMovie)
+
+function addMovie(e) {
+  e.preventDefault()
+  const data = {
+    'title': e.target.title.value,
+    'description': e.target.description.value,
+    'genre': e.target.genre.value,
+    'stars': e.target.stars.value,
+    'Image': e.target.img.value
+  }
+  fetch('http://localhost:3000/movies/', {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body:JSON.stringify({data})
+  }).then((movie)=> {
+    const {id, title, description, genre, image, stars} = movie
+    new Movie(id, title, description, genre, image, stars)
+  })
+
+}
