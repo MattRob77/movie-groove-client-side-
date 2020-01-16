69cee4e9a577cee85e905c4a827e07f74b94fc8b
github username <github email address>
2020-01-16 11:01:28 -0500

    Added id to movie-form to be abvle to grab the data needed to submit new to db

--- src/index.js
+++ src/index.js
@@ -11,20 +11,21 @@ document.querySelector('form').addEventListener('submit', addMovie)
 
 function addMovie(e) {
   e.preventDefault()
-  const data = {
-    'title': e.target.title.value,
-    'description': e.target.description.value,
-    'genre': e.target.genre.value,
-    'stars': e.target.stars.value,
-    'Image': e.target.img.value
-  }
+const data = {
+  'title': e.target.title.value,
+  'description': e.target.description.value,
+  'genre': e.target.genre.value,
+  'stars': e.target.stars.value,
+  'image': e.target.img.value
+}
+
   fetch('http://localhost:3000/movies/', {
     method: "POST",
     headers: {
       'Content-Type': 'application/json'
     },
     body:JSON.stringify({data})
-  }).then((movie)=> {
+  }).then((movie) => {
     const {id, title, description, genre, image, stars} = movie
     new Movie(id, title, description, genre, image, stars)
   })
