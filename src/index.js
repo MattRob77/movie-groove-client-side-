211187a988e30420254f6199ab6ec3db44899794
github username <github email address>
2020-01-23 22:43:01 -0500

    Added comments to index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-fetch('http://localhost:3000/movies')
-.then(resp => resp.json())
+fetch('http://localhost:3000/movies') //returns a promise that (fetches = grabs)
+.then(resp => resp.json()) //resp.json to see javascript data
 .then(movies => {
-  movies.forEach(movie =>{
+  movies.forEach(movie =>{ //provides a callback function()
     const {id, title, description, genre, image, stars} = movie
     new Movie(id, title, description, genre, image, stars)
   })
 })
+//const creates a read only value(will not change)
 
 document.querySelector('form').addEventListener('submit', addMovie)
+
+//querySelector returns first element matching the selector('form')
+//addEventListener method attaches an event handler to the specified element(waits for event)
 
 function addMovie(e) {
   e.preventDefault()
@@ -39,4 +43,4 @@ const data = {
   'content': e.target.content.value,
   'stars': e.target.stars.value,
   }
-} 
+}
