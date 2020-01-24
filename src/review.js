6e257f02277d6b4de4ff68f3651d1e0db3d13f10
github username <github email address>
2020-01-23 19:13:18 -0500

    Wrote class for review

---
+++ src/review.js
@@ -0,0 +1,9 @@
+class Review {
+  constructor(id, title, description, genre, image, stars){
+  this.movie_id = movie_id
+  this.title = title
+  this.content = content
+  this.genre = genre
+  this.stars = stars
+  this.render()
+  }
